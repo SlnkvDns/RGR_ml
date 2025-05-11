4b066a7686c6d27f2b9172566334a079a375c0e4
SlnkvDns <denissalnikov8@gmail.com>
2025-05-11 15:45:35 +0600

    add test widgets on contact page

--- pages/contact_page.py
+++ pages/contact_page.py
@@ -1,3 +1,22 @@
 import streamlit as st
 
-st.write()
+name = st.text_input("Имя разработчика", "Сальников Денис")
+number = st.number_input(
+    label="Вариант",
+    min_value=1,
+    max_value=10,
+    value="min",
+    step=1,
+
+    )
+st.title("Информация о разработчкике")
+st.write("Имя: ", name)
+st.write("Вариант: ", number)
+dataset = st.selectbox(
+    label="Датасет",
+    options=("Регрессия", "Классификация", "Кластеризация"),
+    index=None,
+    placeholder="Выберите датасет"
+)
+
+st.write("Выбранный датасет: ", dataset)
