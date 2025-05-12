10c65d7443728cfdf4cfc89a7a7c93c7bbfc8753
SlnkvDns <denissalnikov8@gmail.com>
2025-05-12 20:04:45 +0600

    text_input -> write

--- pages/contact_page.py
+++ pages/contact_page.py
@@ -5,6 +5,6 @@ GROUP = "ФИТ-231"
 VARIANT = "1"
 
 st.title("Информация о разработчкике")
-st.text_input("Имя разработчика: ", NAME)
-st.text_input("Группа: ", GROUP)
-st.text_input("Вариант датасета: ", VARIANT)
+st.write("Имя разработчика: ", NAME)
+st.write("Группа: ", GROUP)
+st.write("Вариант датасета: ", VARIANT)
