15ef691967b55a2022f1932b22291af53970b6ba
SlnkvDns <denissalnikov8@gmail.com>
2025-05-12 19:40:20 +0600

    delete test code

--- pages/contact_page.py
+++ pages/contact_page.py
@@ -11,6 +11,7 @@ number = st.number_input(
     )
 st.title("Информация о разработчкике")
 st.write("Имя: ", name)
+st.write("Группа: ФИТ-231")
 st.write("Вариант: ", number)
 dataset = st.selectbox(
     label="Датасет",
