5e8f75f2430656c4a6859e41062286e22b69531a
SlnkvDns <denissalnikov8@gmail.com>
2025-05-12 19:57:54 +0600

    fix pages

--- pages/contact_page.py
+++ pages/contact_page.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.write()
+NAME = "Сальников Денис"
+GROUP = "ФИТ-231"
+VARIANT = "1"
+
+st.title("Информация о разработчкике")
+st.text_input("Имя разработчика: ", NAME)
+st.text_input("Группа: ", GROUP)
+st.text_input("Вариант датасета: ", VARIANT)
