0eb773358a493d40fb509027ff597d806ea24347
SlnkvDns <denissalnikov8@gmail.com>
2025-05-12 19:57:54 +0600

    fix pages

--- pages/contact_page.py
+++ pages/contact_page.py
@@ -1,23 +1,10 @@
 import streamlit as st
 
-name = st.text_input("Имя разработчика", "Сальников Денис")
-number = st.number_input(
-    label="Вариант",
-    min_value=1,
-    max_value=10,
-    value="min",
-    step=1,
+NAME = "Сальников Денис"
+GROUP = "ФИТ-231"
+VARIANT = "1"
 
-    )
 st.title("Информация о разработчкике")
-st.write("Имя: ", name)
-st.write("Группа: ФИТ-231")
-st.write("Вариант: ", number)
-dataset = st.selectbox(
-    label="Датасет",
-    options=("Регрессия", "Классификация", "Кластеризация"),
-    index=None,
-    placeholder="Выберите датасет"
-)
-
-st.write("Выбранный датасет: ", dataset)
+st.text_input("Имя разработчика: ", NAME)
+st.text_input("Группа: ", GROUP)
+st.text_input("Вариант датасета: ", VARIANT)
